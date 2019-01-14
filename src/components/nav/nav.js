555b6d2e37be6621c920defa4ca96b64a5cf95cc
benlevine1 <benlevine1@gmail.com>
2019-01-14 15:46:48 -0800

    completed nav and sidenav

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -1,7 +1,8 @@
 import './nav.scss'
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { Link } from 'react-router-dom';
-import { S_IFLNK } from 'constants';
+import SideNav from './side_nav'
+
 
 class Nav extends Component{
   state = {
@@ -41,35 +42,53 @@ class Nav extends Component{
     ]
   }
 
+  buildLink(link){
+    return(
+      <li key={link.to}>
+        <Link to={link.to}>{link.text}</Link>
+      </li>
+    )
+  }
+  handleSignOut = () => {
+    console.log('sign-out button clicked')
+  }
+
   renderLinks(){
     const { authLinks, commonLinks, noAuthLinks } = this.state;
-    const auth = false;
+    const auth = true ;
 
     let links = [...commonLinks];
 
     if(auth){
-      links = [...links, ...authLinks ]
-    } else {
-      links = [ ...links, ...noAuthLinks ];
-    }
+      links = [...links, ...authLinks ].map(this.buildLink);
 
-    return links.map(link=>{
-      return (
-        <li key={link.to}>
-          <Link to={link.to}>{link.text}</Link>
+      links.push(
+        <li key="sign-out">
+          <button onClick = {this.handleSignOut} className="btn black">Sign Out</button>
         </li>
       )
-    })
+    } else {
+      links = [ ...links, ...noAuthLinks ].map(this.buildLink);
+    }
+
+    return links
   }
 
   render(){
+    const links = this.renderLinks();
     return(
-      <nav className="black">
-       <Link className="brand-logo" to="/">Movie Quotes</Link>
-       <ul className="right">
-        {this.renderLinks()}
-       </ul>
-      </nav>
+      <Fragment>
+        <nav className="black">
+          <Link className="brand-logo" to="/">Movie Quotes</Link>
+          <a href="#" data-target="side-nav" className="sidenav-trigger">
+            <i className="material-icons">menu</i>
+          </a>
+          <ul className="right hide-on-med-and-down">
+            {links}
+          </ul>
+        </nav>
+        <SideNav links={links}/>
+      </Fragment>
     )
   }
 }
